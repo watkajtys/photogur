72f1b813538d93f486076bad338a2e75d4f09fb2
Matt Watkajtys <watkajtys@gmail.com>
2013-03-19 14:12:22 -0400

    Initial Commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,5 @@
+class PicturesController < ActionController::Base
+
+	def index
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogur::Application.config.secret_token = 'b0bde759e5c89b7f3f4a8e67519c7b09f5d1c776706025adb356fcfac21674ec8ca906e38af6e80b74913864b84803a5b7b819d84a8a9a025de6da3eb4170832'
