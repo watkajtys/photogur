b4e176f56b2f589c03501d6da8d2cdd01de8337d
Matt Watkajtys <watkajtys@gmail.com>
2013-03-19 14:44:17 -0400

    Pictures controller was incorrect. Switched to inherit from ApplicationController. Added pictures instance variable.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,12 @@
-class PicturesController < ActionController::Base
+class PicturesController < ApplicationController
 
 	def index
 		@greeting = "Hello World"
+
+		@pictures = [
+		'http://monicao.s3.amazonaws.com/bitmaker/house.jpg',
+		'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg',
+		'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg'
+		]
 	end
 end
