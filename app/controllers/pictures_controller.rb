503efe2723a2cc1308b50282e0c00393a4f9da0f
Matt Watkajtys <watkajtys@gmail.com>
2013-03-19 16:11:20 -0400

    Added hashes to pictures instance variable in pictures controller to contain data. Linked that over in the views erb to display the photo, the title, and the artist.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,9 +4,22 @@ class PicturesController < ApplicationController
 		@greeting = "Hello World"
 
 		@pictures = [
-		'http://monicao.s3.amazonaws.com/bitmaker/house.jpg',
-		'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg',
-		'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg'
+			{
+				:title => "The old church on the coast of the white sea",
+				:artist => "Sergey Ershov",
+				:url => 'http://monicao.s3.amazonaws.com/bitmaker/house.jpg'
+			},
+			{
+				:title => "Sea Power",
+				:artist => "Stephen Scullion",
+				:url => 'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg'
+			},
+			{
+				:title => "Poppies",
+				:artist => "John Wilhelm",
+				:url => 'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg'
+
+			}
 		]
 	end
 end
