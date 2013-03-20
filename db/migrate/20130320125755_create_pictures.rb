b706100ee5ff2112b853c8a9fffa9e237081bdaa
Matt Watkajtys <watkajtys@gmail.com>
2013-03-20 09:16:15 -0400

    Added information to create table in migrate. Migrated DB to create schema.

--- db/migrate/20130320125755_create_pictures.rb
+++ db/migrate/20130320125755_create_pictures.rb
@@ -1,5 +1,10 @@
 class CreatePictures < ActiveRecord::Migration
   def up
+  	create_table :pictures do |t|
+  		t.string :artist
+  		t.string :title
+  		t.string :url
+  	end
   end
 
   def down
