6de734fc06419a8c0507b8d4b05e8a61badb9fda
Matt Watkajtys <watkajtys@gmail.com>
2013-03-22 09:08:27 -0400

    Final refactor on validation. Deleted redundant presence. Changed color of error messages.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,7 @@ class Picture < ActiveRecord::Base
 	attr_accessible :title, :artist, :url
 
     validates :title, :presence => true
-    validates :url, {
-        :presence => true, 
+    validates :url, { 
         :format => {
             :with => /^https?:/, :message => "Please Enter a Valid URL"
         }
