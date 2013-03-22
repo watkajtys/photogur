53294f16dbdbd377749d292ee7b5601263b18d46
Matt Watkajtys <watkajtys@gmail.com>
2013-03-22 08:58:38 -0400

    Refactored out extra flash hardcoded into new.html. Also added url check validataion and refactored to make it more legible.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,5 +2,10 @@ class Picture < ActiveRecord::Base
 	attr_accessible :title, :artist, :url
 
     validates :title, :presence => true
-    validates :url, :presence => true
+    validates :url, {
+        :presence => true, 
+        :format => {
+            :with => /^https?:/, :message => "Please Enter a Valid URL"
+        }
+    }
 end
